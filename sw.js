1dd63430a12b12687c09db8c62f104761f68bd66
pgallagher6-png <pgallagher6@gmail.com>
2025-09-15 06:28:46 -0700

    Add service worker for offline support

--- sw.js
+++ sw.js
@@ -1,1 +1,30 @@
+const CACHE_NAME = 'bainbridge-ferry-v1';
+const urlsToCache = [
+    '/',
+    '/index.html'
+];
 
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+// 3. ADD: Register service worker (add to bottom of index.html before closing </script> tag):
+
+// Register service worker for offline support
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then(reg => console.log('Service Worker registered'))
+            .catch(err => console.log('Service Worker registration failed'));
+    });
+}
